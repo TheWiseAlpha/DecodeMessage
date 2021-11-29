7b5d6cc908ad0e3f891553eccc70dd11eb25af5a
Karthik <Karthik@WINDELL-B5D3FDF>
2021-11-29 08:04:42 +0100

    Add project files.

---
+++ DecodeMessage/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTask
+{
+
+    public class MessageDec
+    {
+        public string path { get; set; }
+        public string initial { get; set; }
+
+        public void decodeMessage(string path, string initial)
+        {
+
+            var filepath = Path.Combine(Directory.GetCurrentDirectory(), path);
+            var lines = File.ReadAllText(filepath);
+
+            int counter = 0;
+            StreamReader file = new StreamReader(filepath);
+
+            while ((lines = file.ReadLine()) != null)
+            {
+                if (lines.Contains(initial))
+                {
+                    var msg = lines.ElementAt(4);
+                    Console.WriteLine(msg);
+                    var revstring = lines.Reverse().Take(3).ToArray();
+
+                }
+
+
+                counter++;
+            }
+
+
+
+            file.Close();
+            Console.ReadKey();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MessageDec md = new MessageDec();
+            md.decodeMessage("EncodedMessage.txt", "QVH");
+        }
+    }
+
+}
